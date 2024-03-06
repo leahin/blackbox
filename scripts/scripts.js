1d921caa07bbe4d06b5729d76d9e015164df6576
Leah In <157823606+leahin@users.noreply.github.com>
2024-03-06 17:53:09 -0500

    added launch points

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,32 @@
-function renderBoard(size) {
-  for (let i = 0; i < size; i++) {
+const SIZE = 8;
+
+function renderBlackbox() {
+  const blackbox = document.getElementById('blackbox');
+  for (let i = 0; i < SIZE; i++) {
     let row = document.createElement('div');
     row.className = `row-${i}`;
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < SIZE; j++) {
       let cell = document.createElement('div');
-      cell.className = `cell-${i}-${j}`;
+      cell.className = `cell cell-${i}-${j}`;
       row.appendChild(cell);
     }
-    document.getElementById('board').appendChild(row);
+    blackbox.appendChild(row);
   }
 }
 
-renderBoard(8);
+function renderLaunchPoints(direction) {
+  const launchPointRow = document.getElementById(
+    `launch-point-row-${direction}`
+  );
+  for (let i = 0; i < SIZE; i++) {
+    let launchPoint = document.createElement('div');
+    launchPoint.className = `launch-point launch-point-${direction}-${i}`;
+    launchPointRow.appendChild(launchPoint);
+  }
+}
+
+renderLaunchPoints('top');
+renderLaunchPoints('bottom');
+renderLaunchPoints('left');
+renderLaunchPoints('right');
+renderBlackbox();
